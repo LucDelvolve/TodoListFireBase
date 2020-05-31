64388e78511218d894a03b1693d1b2d241802229
LucDelvolve <31876700+LucDelvolve@users.noreply.github.com>
2020-05-31 10:13:22 +0200

    Update todo.js

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,6 +1,3 @@
-/**
- * 
- */
 export async function fetchTodos() {
   const config = window.config;
   return fetch(`${config.api}/todos`, {
@@ -16,67 +13,49 @@ export async function fetchTodos() {
   });
 }
 
- export function fetchTodo() {
-    const config = window.config;
-  
-    return fetch(`${config.api}/todos`, {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json'
-      }
-    })
-    .then(result => result.json())
-    .catch(error => {
-      console.error(error);
-      return false;
-    })
-  }
+export function fetchTodo() {
+  const config = window.config;
 
-  export function createTodo(data) {
-    const config = window.config;
-    return fetch(`${config.api}/todos`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data)
-    })
-    .then(result => result.json())
-    .catch(error => {
-      console.error(error);
-      return false;
-    }); 
-  }
+  return fetch(`${config.api}/todos`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  .then(result => result.json())
+  .catch(error => {
+    console.error(error);
+    return false;
+  })
+}
 
-  export async function deleteTodo(id) {
-    const config = window.config;
-    return fetch(`${config.api}/todos/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
-    .then(result => result.json())
-    .catch(error => {
-      console.error(error);
-      return false;
-    }); 
-  }
-  
-  export async function updateTodo(data) {
-    const config = window.config;
-    return fetch(`${config.api}/todos/${data.id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data)
-    })
-    .then(result => result.json())
-    .catch(error => {
-      console.error(error);
-      return false;
-    }); 
-  }
-  
-  
+export function createTodo(data) {
+  const config = window.config;
+  return fetch(`${config.api}/todos`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+  .then(result => result.json())
+  .catch(error => {
+    console.error(error);
+    return false;
+  }); 
+}
+
+export async function deleteTodo(id) {
+  const config = window.config;
+  return fetch(`${config.api}/todos/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then((result) => {result.json(); window.location.reload(true)})
+  .catch(error => {
+    console.error(error);
+    return false;
+  }); 
+}
